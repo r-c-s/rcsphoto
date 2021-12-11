b84d977ffe42e2e4d6250c02ae24966ef5ca1d90
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-12-11 08:20:31 -0300

    use cleaned up db

--- routes/album.js
+++ routes/album.js
@@ -2,6 +2,7 @@ var _      = require('underscore');
 var cradle = require('cradle');
 var vcapServices = require('vcap_services');
 var credentials = vcapServices.findCredentials({ service: 'cloudantNoSQLDB' });
+var imageBaseUrl = 'http://d164y11hqgve5j.cloudfront.net';
 
 function dbConnection() {
   return new(cradle.Connection)({
@@ -30,7 +31,7 @@ module.exports.getAlbums = function(req, res) {
       }
       
       res.render('index', { 
-        albums : _.map(data.rows, function(row) { return row.doc; })
+        albums : _.map(data.rows, function(row) { return enrichAlbum(row.doc); })
            .sort(function(a, b) { return a.sortOrder - b.sortOrder; })
       });
     });
@@ -44,6 +45,29 @@ module.exports.getAlbum = function(req, res) {
       return res.render('error', { message : JSON.stringify(error, null, ' ') });
     }
     
-    res.render('album', { album : album });
+    res.render('album', { album : enrichAlbum(album) });
   });
 };
+
+function enrichAlbum(album) {
+  return {
+    id: album.id,
+    name: album.name,
+    coverImage: album.coverImage,
+    sortOrder: album.sortOrder,
+    coverImage: createUrl(album.id, album.coverImage, 'small'),
+    images: _.map(album.images, function(image) {
+      return {
+        thumb: createUrl(album.id, image, 'thumb'),
+        small: createUrl(album.id, image, 'small'),
+        medium: createUrl(album.id, image, 'medium'),
+        large: createUrl(album.id, image, 'large'),
+        full: createUrl(album.id, image, 'full')
+      }
+    })
+  }
+}
+
+function createUrl(albumId, imageName, size) {
+  return imageBaseUrl + '/' + albumId + '/' + size + '/' + imageName;
+}
