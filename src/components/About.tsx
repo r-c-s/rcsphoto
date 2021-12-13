0edd64c1a90d03fb41f3e1ccb0d6211e7e74e8c2
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-12-13 19:33:57 +0000

    remove Boostrap from about because app doesn't use much of it

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ function About() {
         <p>This is a portfolio for showcasing my photography, one of my biggest hobbies once upon a time.</p>
         <p>All images were taken using a Sony DSC-RX100 II and V cameras.</p>
         <p>I shoot mostly in manual and record in raw format. I also use CaptureOne to edit my work.</p>
-        <p>This website was written in React with Typescript and uses the Twitter Bootstrap CSS framework. All photos are uploaded on Amazon S3 and served by CloudFront, the album info is stored on Cloudant DBaaS, and the site is hosted on IBM Bluemix.</p>
+        <p>This website was written in React with Typescript. All photos are uploaded on Amazon S3 and served by CloudFront, the album info is stored on Cloudant DBaaS, and the site is hosted on IBM Bluemix.</p>
         <p>Thanks for viewing!</p>
         <p>The source code can be found on <a href='https://www.github.com/r-c-s/rcsphoto' target='_blank'>GitHub</a>.</p>
         <div className="image-container">
