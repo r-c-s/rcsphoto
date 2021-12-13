c11bb5311725f9da28a4bdee25f03e1a1bbfa67a
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-12-13 08:43:05 +0000

    remove cloudant keys

--- src/index.tsx
+++ src/index.tsx
@@ -14,13 +14,7 @@ const {
 const serviceConfig: ServiceConfig = {
   imageBaseUrl,
   databaseName,
-  credentials: {
-    "username": "24a901e8-b5e7-49ad-a6f2-6cda3a56cacc-bluemix",
-    "password": "d469ac734851f7c24d885b9f1c6c0d466c24367354ca4761c58254096c8fff8b",
-    "host": "24a901e8-b5e7-49ad-a6f2-6cda3a56cacc-bluemix.cloudant.com",
-    "port": 443,
-    "url": "https://24a901e8-b5e7-49ad-a6f2-6cda3a56cacc-bluemix:d469ac734851f7c24d885b9f1c6c0d466c24367354ca4761c58254096c8fff8b@24a901e8-b5e7-49ad-a6f2-6cda3a56cacc-bluemix.cloudant.com"
-  }//JSON.parse(REACT_APP_DB_CREDENTIALS)
+  credentials: JSON.parse(REACT_APP_DB_CREDENTIALS)
 };
 
 ReactDOM.render(
