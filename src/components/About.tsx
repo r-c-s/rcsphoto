9dfc5f73eff74d8c112d50175c0bd98a98a1fb0e
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-12-17 16:11:40 +0000

    simplify about language

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,11 +9,11 @@ function About() {
     <div className="container">
       <PageHeader title="About" subtitle={"by Raphael Corrêa"}/>
       <div className="body">
-        <p>This is a portfolio for showcasing my photography, one of my biggest hobbies once upon a time.</p>
-        <p>All images were taken using a Sony DSC-RX100 II and V cameras.</p>
+        <p>This is a portfolio for my photography, one of my biggest hobbies once upon a time.</p>
+        <p>All images were taken with the Sony DSC-RX100 2 and 5 cameras.</p>
         <p>I shoot mostly in manual and record in raw format. I also use CaptureOne to edit my work.</p>
-        <p>This website was written in React with Typescript. All photos are uploaded on AWS S3 and served by AWS CloudFront, the album info is stored on IBM Cloudant (CouchDB), and the site is hosted on IBM CloudFoundry.</p>
-        <p>The source code can be found on <a href='https://www.github.com/r-c-s/rcsphoto' target='_blank'>GitHub</a>.</p>
+        <p>This website was written in React with Typescript. The pictures are uploaded on AWS S3 and served by AWS CloudFront, the album info is stored on IBM Cloudant, and the site is hosted on IBM CloudFoundry.</p>
+        <p>The source code is on <a href='https://www.github.com/r-c-s/rcsphoto' target='_blank'>GitHub</a>.</p>
         <p>Thanks for viewing!</p>
         <div className="image-container">
           <img 
